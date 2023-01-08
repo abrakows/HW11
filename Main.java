0acbe5ccd1ccbbb5dd445b05ec3504507db0494a
abrakows <105364710+abrakows@users.noreply.github.com>
2023-01-08 13:15:21 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+  public static void main(String[] args){
+    System.out.println("Aufgabe 5");
+    System.out.println("Veränderung für Branch_1 (Addition on Master)");
+  }
+}
