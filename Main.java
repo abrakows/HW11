3565b943718fa9b7ffafb6e1c45431c44e1ebcb7
abrakows <105364710+abrakows@users.noreply.github.com>
2023-01-08 13:35:10 +0100

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-
-  public static void main(String[] args){
-    System.out.println("Aufgabe 5");
-    System.out.println("Veränderung für Branch_1 (Addition on Master) (neue Anderung");
-  }
-}
